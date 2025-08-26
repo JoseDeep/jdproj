88264fcac599c79568a209c7ab2b412097d213c8
Joseph Deepan <josephdeepan.26ec@licet.ac.in>
2025-08-26 15:01:55 +0530

    commit

---
+++ demojose/src/main/java/com/joseee/demojose/DemojoseApplication.java
@@ -0,0 +1,13 @@
+package com.joseee.demojose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojoseApplication.class, args);
+	}
+
+}
---
+++ demojose/src/test/java/com/joseee/demojose/DemojoseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseee.demojose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
